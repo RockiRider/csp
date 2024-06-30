f5950021540dff57dc8244887be8ca9c4c7eb06e
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-06-30 23:13:39 +0100

    add initial mui starter

---
+++ apps/mui/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import csp from "vite-plugin-hash-csp";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    csp({
+      algorithm: "sha256",
+      runOnDev: true,
+    }),
+  ],
+  preview: {
+    port: 4001,
+  },
+  server: {
+    port: 3001,
+  },
+});
