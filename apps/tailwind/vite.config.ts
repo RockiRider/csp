cc6b2c09a9fdaad4929491c2533d1201aaab83ca
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-07-13 18:12:28 +0100

    fix ts issue

--- apps/tailwind/vite.config.ts
+++ apps/tailwind/vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import csp from "vite-plugin-hash-csp";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react() as PluginOption[], //This is a type assertion due to a monorepo issue regarding stylus, this is not needed in a normal project
     csp({
       dev: {
         run: true,
         outlierSupport: ["tailwind"],
       },
       policy: {
-        "style-src-elem": ["'unsafe-inline'"],
+        "style-src-elem": ["'unsafe-inline'"], //For testing purposes on dev
       },
     }),
   ],
