55725bbb80bc9188bceb37d5677224b3de2ae939
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-07-21 14:30:15 +0100

    deal with stylus issue

--- apps/vue-app/vite.config.ts
+++ apps/vue-app/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import vue from "@vitejs/plugin-vue";
 import csp from "vite-plugin-hash-csp";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), csp()],
+  plugins: [vue(), csp({ dev: { run: true } }) as PluginOption], //This is a type assertion due to a monorepo issue regarding stylus, this is not needed in a normal project
   preview: {
     port: 4007,
   },
