d60d357508dcc69b6ab3d9f821b12ba88f0096df
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-08-08 18:24:48 +0100

    make use of terser

--- packages/vite-plugin-hash-csp/rollup.config.mjs
+++ packages/vite-plugin-hash-csp/rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
+import terser from "@rollup/plugin-terser";
 
 export default defineConfig({
   input: "src/index.ts", // Your plugin's entry point
@@ -21,5 +22,6 @@ export default defineConfig({
     }), // Transpile TypeScript
     resolve(), // Resolves node modules
     commonjs(), // Converts commonjs to ES modules
+    terser(), // Minify
   ],
 });
