bd8bcbd28a57b4e31d9e501c9490bc39846cb333
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-08-14 21:32:40 +0100

    update config

--- apps/react/vite.config.ts
+++ apps/react/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import csp from "vite-plugin-hash-csp";
 
-// sha256-CvA/5Jt9LLnZntYQs+tHcL5iw9pe/MwHfvVrF22kwYY=
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,12 +13,6 @@ export default defineConfig({
       },
       build: {
         hash: true,
-      },
-      policy: {
-        "script-src-elem": [
-          "sha256-CvA/5Jt9LLnZntYQs+tHcL5iw9pe/MwHfvVrF22kwYY=",
-          // "'self'",
-        ],
       },
     }),
   ],
