9fe81633f20314d7b1590ebc1293d79bc7fd4e3d
Tsotne Gvadzabia <tsotne@tsotne.co.uk>
2024-12-19 22:39:47 +0000

    v5 added

---
+++ apps/v5/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import csp from "vite-plugin-csp-guard";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    csp({
+      algorithm: "sha256",
+      dev: {
+        run: true,
+      },
+      policy: {
+        "font-src": ["https://fonts.gstatic.com"],
+        "script-src-elem": ["'self'"],
+        "connect-src": ["*"],
+        "object-src": ["'none'"],
+      },
+      override: true,
+    }),
+  ],
+  preview: {
+    port: 4000,
+  },
+  server: {
+    port: 3000,
+  },
+});
