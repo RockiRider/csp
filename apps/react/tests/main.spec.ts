80844bd26f42447b06b349bda8b94a094ee0307b
Tsotne Gvadzabia <tsotne@tsotne.co.uk>
2024-12-28 18:24:26 +0000

    remove override test

--- apps/react/tests/main.spec.ts
+++ apps/react/tests/main.spec.ts
@@ -1,4 +1,4 @@
-import { genericTests, jQueryTest, overrideTest, inlineScriptBlockedTest, viteLogoTest } from '@repo/testing';
+import { genericTests, jQueryTest, inlineScriptBlockedTest, viteLogoTest } from '@repo/testing';
 
 const APP_TITLE = "Vite + React"; 
 const HEADER_COLOUR = "rgb(33, 53, 71)"
@@ -7,6 +7,5 @@ const BTN_COLOUR = "rgb(255, 0, 0)"
 genericTests(APP_TITLE, {headerColour: HEADER_COLOUR, buttonColour: BTN_COLOUR})
 jQueryTest()
 viteLogoTest();
-overrideTest(APP_TITLE)
 inlineScriptBlockedTest(APP_TITLE)
 
--- apps/react/vite.config.ts
+++ apps/react/vite.config.ts
@@ -19,7 +19,6 @@ export default defineConfig({
       build: {
         sri: true
       },
-      override: true,
     }),
   ],
   preview: {
